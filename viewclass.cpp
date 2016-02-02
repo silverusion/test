bf9d8b3a9bc279037d72fdb550c9c8f5ce667b2d
silverusion <larik94@inbox.ru>
2016-02-02 22:20:14 +0300

    второй

--- viewclass.cpp
+++ viewclass.cpp
@@ -9,10 +9,7 @@ void ViewClass::wheelEvent(QWheelEvent *event)
     setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
     double scaleFactor = 1.1;
 
-    if(event->delta() > 0)
-    {
-        scale(scaleFactor, scaleFactor);
-    }
+
     else
     {
         scale(1/scaleFactor, 1/scaleFactor);
